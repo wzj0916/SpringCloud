861dd429bb540b05eca190fd3f357a86c677ba9d
wzj0916 <1336720773@qq.com>
2019-09-16 15:30:06 +0800

    提交了springclouddemo

---
+++ basespringcloudemo1/src/main/java/com/itszt8/baseclouddemo/BaseCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.itszt8.baseclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseCloudDemoApplication.class, args);
+	}
+
+}
---
+++ basespringcloudemo1/src/main/java/com/itszt8/baseclouddemo/entities/Order.java
@@ -0,0 +1,26 @@
+package com.itszt8.baseclouddemo.entities;
+
+public class Order {
+
+    private String id;
+    private String detail;
+
+    public Order() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
